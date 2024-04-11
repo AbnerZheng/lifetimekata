1cae77ca348c00b8144404fe00990696ba8bb7bf
Abner Zheng <abnerzheng@gmail.com>
2024-04-11 10:03:54 +0800

    finish ex3

--- exercises/03_lifetime_elision/exercise/src/lib.rs
+++ exercises/03_lifetime_elision/exercise/src/lib.rs
@@ -1,21 +1,21 @@
 use require_lifetimes::require_lifetimes;
 
 #[require_lifetimes(!)]
-pub fn example_a(_number: &i32) -> (&i32, &i32) {
+pub fn example_a<'elided1>(_number: &'elided1 i32) -> (&'elided1 i32, &'elided1 i32) {
     unimplemented!()
 }
 
 #[require_lifetimes(!)]
-pub fn example_b(_first_arg: &i32, _second_arg: &i32, _third_arg: &Option<&i32>) {
+pub fn example_b<'elided1, 'elided2, 'elided3, 'elided4>(_first_arg: &'elided1 i32, _second_arg: &'elided2 i32, _third_arg: &'elided3 Option<&'elided4 i32>) {
     unimplemented!()
 }
 
 #[require_lifetimes(!)]
-pub fn example_c<'a>(_first_arg: &'a i32, _second_arg: &'a i32) -> &i32 {
+pub fn example_c<'a>(_first_arg: &'a i32, _second_arg: &'a i32) -> &'a i32 {
     unimplemented!()
 }
 
 #[require_lifetimes(!)]
-pub fn example_d<'a>(_first_arg: &'a i32, _second_arg: &i32) -> &'a i32 {
+pub fn example_d<'a, 'b>(_first_arg: &'a i32, _second_arg: &'b i32) -> &'a i32 {
     unimplemented!()
 }
